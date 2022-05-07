8c2fd424147a07b3ed65e25211c149d86c459270
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-05-07 19:22:35 +0200

    sett change

--- src/Planningator/settings.py
+++ src/Planningator/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     #Renvoyer une URl
     'planning_app',
     #créer une URL
@@ -127,6 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = "./static_root"
 STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
