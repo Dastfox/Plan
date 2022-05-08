c60163be393bc2e92af598bdc5fd99bddcab934d
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-05-08 17:11:15 +0200

    dld sqlite

--- src/Planningator/settings.py
+++ src/Planningator/settings.py
@@ -10,17 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import dotenv
 import os
 from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
