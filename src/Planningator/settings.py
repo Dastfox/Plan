013cade4e6e7bee1b17403624a497e20564bc179
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-05-08 16:36:26 +0200

    dld sqlite

--- src/Planningator/settings.py
+++ src/Planningator/settings.py
@@ -80,16 +80,25 @@ WSGI_APPLICATION = 'Planningator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+DATABASES = {     
+'default': {
+ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ 'NAME': 'dummy_db',
+ 'USER': 'hero',
+ 'PASSWORD': '',
+ 'HOST': 'localhost',
+ 'PORT': '5432'  
+} 
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
