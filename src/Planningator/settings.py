5b3ec0814c5d92dc40f44510f58b1765cadc3b39
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-05-08 18:03:17 +0200

    dj data

--- src/Planningator/settings.py
+++ src/Planningator/settings.py
@@ -95,7 +95,9 @@ import dj_database_url
 # }
 
 DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
