19e0351c1de454ba80afee5f1786ae6d2c1fc1fa
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-05-09 20:39:41 +0200

    reset

--- src/Planningator/settings.py
+++ src/Planningator/settings.py
@@ -101,9 +101,16 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+if db_from_env: DATABASES['default'].update(db_from_env)
+else : DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
